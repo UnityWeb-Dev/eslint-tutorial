2bdf48ff40b504bea6a19aa47c8c2b9a4022107a
Cap92 <105722701+MathieuAudibert@users.noreply.github.com>
2024-06-21 21:29:40 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,1 +1,21 @@
-
+module.exports = {
+  root: true,
+  parserOptions: {
+    ecmaVersion: 2021, // Use the latest ECMAScript version
+    sourceType: 'module', // Allow the use of imports
+  },
+  env: {
+    node: true, // Node.js global variables and Node.js scoping
+    browser: true, // browser global variables
+    es2021: true, // adds all ECMAScript 2021 globals and automatically sets the ecmaVersion parser option to 12
+  },
+  extends: [
+    'eslint:recommended', // Use ESLint's recommended rules
+    // Add more configurations or plugins here
+  ],
+  rules: {
+    // Customize your rules here
+    // Example:
+    // 'no-console': 'off', // Allows the use of console.log() in development
+  },
+};
